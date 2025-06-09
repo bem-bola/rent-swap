322c8c02208c402d0b534ba9097c0f23f95ed8c6
Bolain <bolain@MacBook-Pro-de-Bolain.local>
2025-06-09 12:00:00 +0200

    Rate limiter

--- src/Controller/LegislationController.php
+++ src/Controller/LegislationController.php
@@ -12,14 +12,24 @@ final class LegislationController extends AbstractController
     #[Route('/mentions-legales', name: 'mentions_legales')]
     public function mentions(): Response
     {
-        return $this->render('legislation/mentions_legales.html.twig', [
-        ]);
+        return $this->render('legislation/mentions_legales.html.twig');
     }
 
     #[Route('/conditions-generales-de-vente-et-d-utilisation', name: 'cgvu')]
     public function cvgu(): Response
     {
-        return $this->render('legislation/cgvu.html.twig', [
-        ]);
+        return $this->render('legislation/cgvu.html.twig');
+    }
+
+    #[Route('/politique-de-confidentialite-et-respect-vie-privee', name: 'politique')]
+    public function politique(): Response
+    {
+        return $this->render('legislation/politique.html.twig');
+    }
+
+    #[Route('/cookies', name: 'cookies')]
+    public function cookie(): Response
+    {
+        return $this->render('legislation/cookies.html.twig');
     }
 }
